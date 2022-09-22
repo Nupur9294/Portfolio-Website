08c4d0969518f7ef00ac21e822dddbba31a34859
Nupur Sharma <nupur9294@gmail.com>
2022-09-22 18:00:45 +0200

    mobile responsive burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log("Hi! Welcome to My Portfolio Site");
+console.log('Hi! Welcome to My Portfolio Site');
+
+function menuToggle() {
+  var x = document.getElementById('myNavToggle');
+
+  if (x.className === 'navtoggle row') x.className += ' responsive row';
+  else x.className = 'navtoggle row';
+}
+
+$(document).ready(function () {
+  $('.fade').css('display', 'none');
+  $('.fade').fadeTo(2000, 1);
+});
